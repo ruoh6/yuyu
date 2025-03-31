b1fba02a1faf5ac95d27328c511a262e91246c3a
hwl <ruoh60662@gmail.com>
2025-03-31 23:10:55 +0800

    Add semaphore class.
    Signed-off-by: hwl <ruoh60662@gmail.com>

--- tests/test_thread.cc
+++ tests/test_thread.cc
@@ -2,11 +2,16 @@
 
 yuyu::Logger::ptr g_logger = YUYU_LOG_ROOT();
 
+int count = 0;
+
 void fun1(){
     YUYU_LOG_INFO(g_logger) << "name: " << yuyu::Thread::GetName()
                             << " this.name: " << yuyu::Thread::GetThis()->getName()
                             << " id: " << yuyu::GetThreadId()
                             << " this.id: " << yuyu::Thread::GetThis()->getId();
+    for (int i = 0; i < 100; i++) {
+        count++;
+    }
 }
 
 void fun2(){
@@ -26,5 +31,6 @@ int main(int argc, char** argv) {
     }
 
     YUYU_LOG_INFO(g_logger) << "Thread test end";
+    YUYU_LOG_INFO(g_logger) << "count=" << count;
     return 0;
 }
--- yuyu/thread.cc
+++ yuyu/thread.cc
@@ -9,6 +9,29 @@ static thread_local std::string t_thread_name = "UNKWON";
 
 static yuyu::Logger::ptr g_logger = YUYU_LOG_NAME("system");
 
+Semaphore::Semaphore(uint32_t count) {
+    if (sem_init(&m_semaphore, 0, count)) {
+        throw std::logic_error("sem_init error");
+    }
+}
+
+Semaphore::~Semaphore() {
+    sem_destroy(&m_semaphore);
+}
+
+void Semaphore::wait() {
+    if (sem_wait(&m_semaphore)) {
+        throw std::logic_error("sem_wait error");
+        return;
+    }
+}
+
+void Semaphore::notify() {
+    if (sem_post(&m_semaphore)) {
+        throw std::logic_error("sem_post error");
+    }
+}
+
 Thread* Thread::GetThis() {
     return t_thread;
 }
@@ -39,6 +62,7 @@ Thread::Thread(std::function<void()> cb, const std::string& name)
             << "name=" << name;        
         throw std::logic_error("pthread_create error");
     }
+    m_semaphore.wait();
 }
 
 Thread::~Thread() {
@@ -68,6 +92,7 @@ void* Thread::run(void* arg) {
     std::function<void()> cb;
     cb.swap(thread->m_cb);
 
+    thread->m_semaphore.notify();
     cb();
     return 0;
 }
